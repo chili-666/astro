8f84af45071abca0e4117b60258dbc6c7b33b9ea
Luis Wäsler <45040285+chili-666@users.noreply.github.com>
2023-03-17 20:34:25 +0000

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.luis-backt-und-kocht.de',
   integrations: [tailwind(), mdx(), preact(), react()],
 });
